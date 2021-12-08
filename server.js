f85a0ccd1783bc3c747bc0227c6a36f180da3a07
mytoandeptrai <mytoandn@gmail.com>
2021-12-08 15:17:00 +0700

    make it betterr

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //socket
 const http = require("http").createServer(app);
